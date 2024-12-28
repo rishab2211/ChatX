8f80195d2575126444428a44df530ca7ba6b5bfe
Rishab raj <rishabraj2211@gmail.com>
2024-12-28 14:12:38 +0530

    added profile info at chat header

--- client/src/pages/chat/components/chat-container/components/chat-header/index.jsx
+++ client/src/pages/chat/components/chat-container/components/chat-header/index.jsx
@@ -1,25 +1,63 @@
 import React from 'react'
 import { RiCloseFill } from "react-icons/ri"
 import { useAppStore } from '../../../../../../store'
+import { Avatar, AvatarImage } from "@/components/ui/avatar"
+import { HOST } from '../../../../../../utils/constants'
 
 const ChatHeader = () => {
 
-    const {closeChat} = useAppStore();
+    const { closeChat, selectedChatData, selectedChatType } = useAppStore();
 
 
-    
+
     return (
         <div className=' h-[10vh] border-b-2 border-[#2f303b] flex items-center justify-between px-20  ' >
             <div className='flex gap-5 items-center  ' >
                 <div className='flex gap-3 items-center justify-center ' >
-                    <div className=' flex justify-center items-center gap-5  ' >
-                        <button className='dark:text-neutral-500 text-neutral-700 focus:neutral-800 focus:border-none focus:outline-none dark:focus:text-neutral-300 duration-300 transition-all '
-                        onClick={closeChat}   >
+                    <div className=' w-12 h-12 relative flex gap-5  ' onClick={() => seletNewselectedChatData(selectedChatData)}  >
+                        <Avatar className=" h-12 w-12 rounded-full overflow-hidden ">
+                            {selectedChatData.image ? (
+                                <AvatarImage
+                                    src={`${HOST}/${selectedChatData.image}`}
+                                    alt="profile-image"
+                                    className=" object-cover w-full h-full "
+
+                                />
+                            ) : (
+                                <div
+                                    className={`uppercase h-12 w-12 text-lg border-[1px] flex items-center justify-center ${getColor(
+                                        selectedChatData.color
+                                    )} `}
+                                >
+                                    {selectedChatData.firstName
+                                        ? selectedChatData.firstName.split("").shift()
+                                        : selectedChatData.email.split("").shift()}
+                                </div>
+                            )}
+                        </Avatar>
+                        <div className='dark:text-white text-black font-semibold '>
+                        {
+                            selectedChatType === "contact" && selectedChatData.firstName? `${selectedChatData.firstName} ${selectedChatData.lastName}` :
+                            selectedChatData.email
+                        }
+                        </div>
+
+                        <div className=' flex justify-center items-center gap-5  ' >
+
+                        
+                    </div>
+                    
+                        
+                    </div>
+                    
+                    
+                </div>
+                
+            </div>
+            <button className='dark:text-neutral-500 text-neutral-700 focus:neutral-800 focus:border-none focus:outline-none dark:focus:text-neutral-300 duration-300 transition-all '
+                            onClick={closeChat}   >
                             <RiCloseFill className='text-3xl' />
                         </button>
-                    </div>
-                </div>
-            </div>
         </div>
     )
 }
--- client/src/pages/chat/components/empty-chat-container/index.jsx
+++ client/src/pages/chat/components/empty-chat-container/index.jsx
@@ -4,14 +4,14 @@ import { animationDefaultOptions } from '../../../../lib/utils';
 
 const EmptyChatContainer = () => {
     return (
-        <div className=' flex-1 md:bg-[#1c1d25] hidden md:flex flex-col justify-center items-center duration-1000 transition-all ' >
+        <div className=' flex-1 hidden md:flex flex-col justify-center items-center duration-1000 transition-all ' >
             <Lottie
             isClickToPauseDisabled={true}
             height={200}
             width={200}
             options={animationDefaultOptions}
             />
-            <div className=' text-opacity-80 text-white flex flex-col gap-10 items-center mt-10 lg:text-4xl text-3xl transition-all duration-300 text-center  ' >
+            <div className=' text-opacity-80 text-black dark:text-white dark: flex flex-col gap-10 items-center mt-10 lg:text-4xl text-3xl transition-all duration-300 text-center  ' >
                 <h3 className='poppins-medium' >
                     Hi<span className='text-purple-500' >!</span> Welcome to 
                     <span className='text-purple-500' > ChatX.</span>
